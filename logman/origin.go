2183276f3b26823c3a2aa2f6c0255a4006bce770
若海 <wang@rehiy.com>
2026-02-07 15:03:54 +0000

    feat: 添加日志格式化函数
    为 Debug、Info、Warn、Error 和 Fatal 添加对应的格式化函数 Debugf、Infof、Warnf、Errorf 和 Fatalf

--- logman/origin.go
+++ logman/origin.go
@@ -1,6 +1,7 @@
 package logman
 
 import (
+	"fmt"
 	"log/slog"
 	"os"
 
@@ -14,9 +15,28 @@ var (
 	Error = slog.Error
 )
 
-func Fatal(msg string, args ...any) {
-	onquit.CallQuitFuncs() // 调用所有退出函数
+func Debugf(msg string, args ...any) {
+	Debug(fmt.Sprintf(msg, args...))
+}
 
+func Infof(msg string, args ...any) {
+	Info(fmt.Sprintf(msg, args...))
+}
+
+func Warnf(msg string, args ...any) {
+	Warn(fmt.Sprintf(msg, args...))
+}
+
+func Errorf(msg string, args ...any) {
+	Error(fmt.Sprintf(msg, args...))
+}
+
+func Fatal(msg string, args ...any) {
+	onquit.CallQuitFuncs()
 	Error(msg, args...)
 	os.Exit(1)
+}
+
+func Fatalf(msg string, args ...any) {
+	Fatal(fmt.Sprintf(msg, args...))
 }
